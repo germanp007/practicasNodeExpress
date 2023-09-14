622d2c78a0d13606468004824e2aea8768da76f3
germanp007 <pintogerman281@gmail.com>
2023-09-14 12:58:03 -0300

    practica

--- src/utils.js
+++ src/utils.js
@@ -3,10 +3,10 @@ import { fileURLToPath } from "url";
 import multer from "multer";
 
 export const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
+console.log(path.join(__dirname, "/public/images"));
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../public/images"));
+    cb(null, path.join(__dirname, "/public/images"));
   },
 
   filename: (req, file, cb) => {
