998c685b792e1b01c1e119449a4fd3c526c700b4
German Pinto <93947560+germanp007@users.noreply.github.com>
2023-11-03 19:00:32 -0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from "url";
 import multer from "multer";
 
 export const __dirname = path.dirname(fileURLToPath(import.meta.url));
-console.log(path.join(__dirname, "/public/images"));
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, "/public/images"));
